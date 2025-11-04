f88986e911c1ada19c21b689dc7703561e8e3025
Danish Ali <D.A.Shaikh10@gmail.com>
2025-11-04 23:04:15 +0100

    WIP: BE - v0.1.0 - Add `proxy` \ middleware.

---
+++ proxy.ts
@@ -0,0 +1,40 @@
+import Negotiator from "negotiator";
+
+import { match } from "@formatjs/intl-localematcher";
+import { NextRequest, NextResponse } from "next/server";
+
+import { DEFAULT_LOCALE, LOCALES } from "@/constants/appsettingConstants";
+
+function getLocale(headers: Headers) {
+  const negotiatorHeaders: Record<string, string> = {};
+  headers.forEach((value, key) => (negotiatorHeaders[key] = value));
+
+  const languages = new Negotiator({ headers: negotiatorHeaders }).languages();
+
+  return match(languages, LOCALES, DEFAULT_LOCALE);
+}
+
+export default function proxy(request: NextRequest) {
+  // Check if there is any supported locale in the pathname
+  const { pathname } = request.nextUrl;
+  const pathnameHasLocale = LOCALES.some(
+    (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
+  );
+
+  if (pathnameHasLocale) return;
+
+  // Redirect if there is no locale
+  const locale = getLocale(request.headers);
+  request.nextUrl.pathname = `/${locale}${pathname}`;
+  console.log(request.nextUrl.pathname);
+  // e.g. incoming request is /products
+  // The new URL is now /en-US/products
+  return NextResponse.redirect(request.nextUrl);
+}
+
+export const config = {
+  matcher: [
+    // Exclude API routes, static files, image optimizations, and .png files
+    "/((?!api|_next/static|_next/image|.*\\.svg|.*\\.ico$).*)",
+  ],
+};
