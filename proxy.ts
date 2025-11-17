78769eeb0d395f79dcb31e4557349a3ff1663fc1
Danish Ali <D.A.Shaikh10@gmail.com>
2025-11-17 22:49:14 +0100

    BE - v0.1.0 - Update `proxy` to allow serving `*.webp` images wihout middleware check.

--- proxy.ts
+++ proxy.ts
@@ -35,6 +35,6 @@ export default function proxy(request: NextRequest) {
 export const config = {
   matcher: [
     // Exclude API routes, static files, image optimizations, and .png files
-    "/((?!api|_next/static|_next/image|.*\\.svg|.*\\.ico$).*)",
+    "/((?!api|_next/static|_next/image|.*\\.svg|.*\\.webp|.*\\.ico$).*)",
   ],
 };
