1dd3c6eea84131237fab9397bbcbc7b3ad393cac
Danish Ali <D.A.Shaikh10@gmail.com>
2026-02-02 10:24:58 +0100

    fix(seo): fix unreachable robots.txt issue - static `robots.txt` should sit in public folder instead of `app` - we fix this issue by using nextjs dynamic `robots.txt` generation - also skip proxy checks on the file

--- proxy.ts
+++ proxy.ts
@@ -18,7 +18,7 @@ export default function proxy(request: NextRequest) {
   // Check if there is any supported locale in the pathname
   const { pathname } = request.nextUrl;
   const pathnameHasLocale = LOCALES.some(
-    (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
+    (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`,
   );
 
   if (pathnameHasLocale) return;
@@ -34,7 +34,7 @@ export default function proxy(request: NextRequest) {
 
 export const config = {
   matcher: [
-    // Exclude API routes, static files, image optimizations, and .png files
-    "/((?!api|_next/static|_next/image|.*\\.svg|.*\\.webp|.*\\.ico$).*)",
+    // Exclude API routes, static files, image optimizations, robots.txt, and .png files
+    "/((?!api|_next/static|_next/image|robots.txt|.*\\.svg|.*\\.webp|.*\\.ico$).*)",
   ],
 };
